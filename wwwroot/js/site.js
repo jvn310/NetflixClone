6d77fb32eabf390267fa69f30a612f1ea30e2547
tumelo <132863300+jvn310@users.noreply.github.com>
2024-10-10 09:27:44 +0200

    added footer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -105,6 +105,24 @@ function prevSlide() {
     }
 }
 
+//Faq questions interactivity
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const parent = question.parentElement;
+
+        // Toggle the active class
+        parent.classList.toggle('active');
+
+        // Close any other open FAQs
+        document.querySelectorAll('.faq-item').forEach(item => {
+            if (item !== parent) {
+                item.classList.remove('active');
+            }
+        });
+    });
+});
+
+
 // Initial call to populate content when the page loads
 handleRegionChange();
 
