3728728c4122cc24fe625f3d1c0f4aeeb3da7ed1
tumelo <132863300+jvn310@users.noreply.github.com>
2024-11-13 11:01:49 +0200

    created a new page and added styling, also adjusted styling of the other pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -146,6 +146,12 @@ namespace NetflixClone.Controllers
             return View(); 
         }
 
+        public ActionResult ChoosePlan()
+        {
+            ViewData["BodyClass"] = "white-background";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
