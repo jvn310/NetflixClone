636b8ce4441c956b73f2adc559ca0ceb17f1c8e3
tumelo <132863300+jvn310@users.noreply.github.com>
2024-11-15 13:50:14 +0200

    added who is watching page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -183,6 +183,11 @@ namespace NetflixClone.Controllers
             return View();
         }
 
+        public ActionResult WhoIsWatching()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
